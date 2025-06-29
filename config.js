0ae3b913a22cfe0d42409bcecc67ee23db7af324
DybyTech <dybytech00@gmail.com>
2025-06-28 21:54:06 -0400

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put your session id here",
 // add your Session Id 
+REJECT_MSG: process.env.REJECT_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
+// msg anticall
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
